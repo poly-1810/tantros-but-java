6e675a6db9f1b0b206531ecbf1fcf32a1bca1cb1
Poly-1810 <105131270+poly-1810@users.noreply.github.com>
2023-01-31 19:22:07 -0600

    editing some funky stuff waow, lets hope this works

--- src/tantros/content/blocks.java
+++ src/tantros/content/blocks.java
@@ -1,4 +1,32 @@
 package tantros.content;
+import mindustry.content.Items;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.meta.BuildVisibility;
 
-public class blocks {
+import static mindustry.type.ItemStack.with;
+
+public class TantrosBlocks {
+    public static Block
+
+    // region Misc
+
+    //cores
+    corePod,
+
+    // endregion
+
+    public static void load(){
+
+
+        // region Misc
+        corePod = new CoreBlock("core-pod"){{
+            requirements(Category.effect, BuildVisibility.editorOnly, with(Items.copper, 1));
+
+        }}
+
+
+    }
 }
