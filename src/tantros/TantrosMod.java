d75cc584e843969da9ced35e1bef78e05afeea2f
MeiNanziiii <oplotDev@gmail.com>
2023-02-02 20:57:28 +0200

    Fixes, Cobalt, Translations, Better class names

--- src/tantros/TantrosMod.java
+++ src/tantros/TantrosMod.java
@@ -1,7 +1,8 @@
 package tantros;
 
 import mindustry.mod.Mod;
-import tantros.content.Blocks;
+import tantros.content.TBlocks;
+import tantros.content.TItems;
 import tantros.content.Tantros;
 
 public class TantrosMod extends Mod {
@@ -9,7 +10,8 @@ public class TantrosMod extends Mod {
 
     @Override
     public void loadContent() {
-        Blocks.load();
+        TItems.load();
+        TBlocks.load();
         Tantros.load();
     }
 }
