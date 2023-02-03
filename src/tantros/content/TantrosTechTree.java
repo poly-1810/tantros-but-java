d40edcd07dc39e3b4920449494c3b2411a2d9ca0
MeiNanziiii <oplotDev@gmail.com>
2023-02-03 12:26:01 +0200

    dead internet

--- src/tantros/content/TantrosTechTree.java
+++ src/tantros/content/TantrosTechTree.java
@@ -2,13 +2,19 @@ package tantros.content;
 
 import mindustry.content.Planets;
 
-import static mindustry.content.TechTree.nodeRoot;
+import static mindustry.content.TechTree.*;
 import static tantros.content.TBlocks.corePod;
 
 public class TantrosTechTree {
     public static void load() {
         Planets.tantros.techTree = nodeRoot("tantros", corePod, true, () -> {
-            
+            nodeProduce(TItems.tantrosCopper, () -> {
+                node(TItems.zinc, () -> {
+                   node(TItems.brass, () -> {
+                       
+                   });
+                });
+            });
         });
     }
 }
