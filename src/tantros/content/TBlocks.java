bd0e35b81e156a25a9b8dc74a55426261ff0e3f2
Poly-1810 <105131270+poly-1810@users.noreply.github.com>
2023-02-05 18:43:12 -0600

    completely forgot to add in the item transportation system, but here we go

--- src/tantros/content/TBlocks.java
+++ src/tantros/content/TBlocks.java
@@ -3,6 +3,8 @@ import mindustry.content.Items;
 import mindustry.content.UnitTypes;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.payloads.PayloadConveyor;
+import mindustry.world.blocks.payloads.PayloadRouter;
 import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.meta.BuildVisibility;
 
@@ -11,14 +13,16 @@ import static mindustry.type.ItemStack.with;
 public class TBlocks {
     public static Block
 
-    // region Misc
 
-    // storage
-    corePod;
-    // endregion
+    // misc
+    corePod,
+    //endregion
+
+    // distribution
+    payloadBelt, payloadRouter,
 
     public static void load() {
-        //region storage
+        //region misc
 
         corePod = new CoreBlock("core-pod"){{
             requirements(Category.effect, BuildVisibility.editorOnly, with(Items.copper, 1));
@@ -31,6 +35,29 @@ public class TBlocks {
             size = 2;
             unitCapModifier = 5;
         }};
+
+        //endregion
+        //region distribution
+        payloadBelt = new PayloadConveyor("payload-belt") {{
+            requirements(Category.units, with(Items.copper, 1));
+            moveTime = 25f;
+            canOverdrive = false;
+            health = 100;
+            researchCostMultiplier = 2f;
+            underBullets = true;
+            size = 2;
+        }};
+
+        payloadRouter = new PayloadRouter("payload-router") {{
+            requirements(Category.units, with(Items.copper, 1));
+            moveTime = 25f;
+            canOverdrive = false;
+            health = 100;
+            researchCostMultiplier = 2f;
+            underBullets = true;
+            size = 2;
+        }};
+
 
 
     }
