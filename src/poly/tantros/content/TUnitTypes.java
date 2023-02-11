3baf05d78daf6ab43d4c81795a4231cf12d4bf09
MeiNanziiii <oplotDev@gmail.com>
2023-02-11 17:19:46 +0200

    submarine

--- src/poly/tantros/content/TUnitTypes.java
+++ src/poly/tantros/content/TUnitTypes.java
@@ -2,11 +2,15 @@ package poly.tantros.content;
 
 import arc.graphics.Blending;
 import arc.graphics.Color;
+import arc.math.Interp;
 import mindustry.content.Fx;
 import mindustry.entities.abilities.ArmorPlateAbility;
 import mindustry.entities.abilities.MoveEffectAbility;
+import mindustry.entities.bullet.BasicBulletType;
 import mindustry.entities.bullet.BombBulletType;
+import mindustry.entities.effect.ParticleEffect;
 import mindustry.entities.part.RegionPart;
+import mindustry.entities.pattern.ShootAlternate;
 import mindustry.entities.pattern.ShootPattern;
 import mindustry.gen.EntityMapping;
 import mindustry.gen.UnitEntity;
@@ -15,12 +19,18 @@ import mindustry.type.Weapon;
 import mindustry.type.ammo.ItemAmmoType;
 
 public class TUnitTypes {
-    public static UnitType trident;
+    public static UnitType
+
+    // core
+    trident,
+
+    // submarines
+    requiem
+    ;
 
     public static void load() {
         trident = new UnitType("trident"){{
             constructor = UnitEntity::create;
-            coreUnitDock = true;
 
             EntityMapping.nameMap.put(name, constructor);
 
@@ -52,7 +62,7 @@ public class TUnitTypes {
             engineOffset = 0f;
             engineSize = 0f;
             armor = 6f;
-            parts.add(new RegionPart("-glow"){{
+            parts.addAll(new RegionPart("-glow"){{
                 color = Color.valueOf("4a4b5300");
                 colorTo = Color.valueOf("4a4b5366");
                 layer = -1;
@@ -93,7 +103,7 @@ public class TUnitTypes {
                    rangeOverride = 48f;
                 }};
             }});
-            abilities.add(new MoveEffectAbility(){{
+            abilities.addAll(new MoveEffectAbility(){{
                 x = 0;
                 y = -5f;
                 rotateEffect = true;
@@ -104,6 +114,110 @@ public class TUnitTypes {
             }}, new ArmorPlateAbility(){{
                 healthMultiplier = 0.5f;
                 color = Color.valueOf("ffd37f");
+            }});
+        }};
+
+        requiem = new UnitType("requiem"){{
+            constructor = UnitEntity::create;
+
+            EntityMapping.nameMap.put(name, constructor);
+
+            envEnabled = 4;
+            health = 350f;
+            flying = true;
+            speed = 1.1f;
+            accel = 0.08f;
+            drag = 0.025f;
+            hitSize = 17f;
+            lowAltitude = true;
+            rotateSpeed = 5f;
+            outlineColor = Color.valueOf("4a4b53");
+            healColor = Color.valueOf("8ca9e8");
+            engineOffset = 0f;
+            engineSize = 0f;
+            armor = 7f;
+            weapons.add(new Weapon("poly-tantros-requiem-silo"){{
+                reload = 50f;
+                rotate = false;
+                mirror = true;
+                alternate = false;
+                x = 8.75f;
+                y = -1.5f;
+                shootY = 0.5f;
+                layerOffset = -5f;
+                baseRotation = -70f;
+                shootCone = 360f;
+                shootWarmupSpeed = 0.2f;
+                minWarmup = 0.8f;
+                parts.addAll(new RegionPart("-side"){{
+                    mirror = true;
+                    moveX = 3.75f;
+                    moveY = -1f;
+                    layerOffset = 1f;
+                    progress = PartProgress.warmup;
+                }});
+                shoot = new ShootAlternate(){{
+                   shots = 2;
+                   barrels = 2;
+                   spread = 3.5f;
+                }};
+                bullet = new BasicBulletType(){{
+                   damage = 8.5f;
+                   splashDamage = 15f;
+                   splashDamageRadius = 24f;
+                   speed = 2f;
+                   drag = -0.03f;
+                   width = 11f;
+                   height = 16f;
+                   lifetime = 40f;
+                   frontColor = Color.valueOf("ffffff");
+                   hitColor = Color.valueOf("8ca9e8");
+                   backColor = Color.valueOf("8ca9e8");
+                   trailColor = Color.valueOf("8ca9e8");
+                   trailWidth = 2f;
+                   trailLength = 6;
+                   homingPower = 0.2f;
+                   homingRange = 80f;
+                   homingDelay = 10f;
+                   despawnEffect = new ParticleEffect(){{
+                       colorFrom = Color.valueOf("8ca9e8");
+                       colorTo = Color.valueOf("8ca9e800");
+                       interp = Interp.pow4Out;
+                       particles = 6;
+                       length = 24f;
+                       sizeFrom = 6f;
+                       sizeTo = 0f;
+                       lifetime = 70f;
+                   }};
+                   hitEffect = new ParticleEffect(){{
+                       colorFrom = Color.valueOf("8ca9e8");
+                       colorTo = Color.valueOf("8ca9e800");
+                       interp = Interp.pow4Out;
+                       particles = 6;
+                       length = 24f;
+                       sizeFrom = 6f;
+                       sizeTo = 0f;
+                       lifetime = 70f;
+                   }};
+                   rangeOverride = 68f;
+                }};
+            }});
+            abilities.addAll(new MoveEffectAbility(){{
+                x = 6;
+                y = -9;
+                rotateEffect = true;
+                interval = 4;
+                teamColor = true;
+                effect = Fx.disperseTrail;
+                parentizeEffects = false;
+            }}, new MoveEffectAbility(){{
+                x = -6;
+                y = -9;
+                rotateEffect = true;
+                interval = 4;
+                teamColor = true;
+                effect = Fx.disperseTrail;
+                parentizeEffects = false;
             }});
         }};
     }
