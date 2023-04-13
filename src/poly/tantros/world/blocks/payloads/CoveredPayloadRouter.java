2e21e03b1b65c0f6c2c457e6bf16ec295131e301
MEEP of Faith <meepmatthew@gmail.com>
2023-04-13 13:55:06 -0700

    Don't rotate the cover region in placement previews

--- src/poly/tantros/world/blocks/payloads/CoveredPayloadRouter.java
+++ src/poly/tantros/world/blocks/payloads/CoveredPayloadRouter.java
@@ -3,11 +3,13 @@ package poly.tantros.world.blocks.payloads;
 import arc.Core;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.TextureRegion;
+import arc.util.*;
+import mindustry.entities.units.*;
 import mindustry.graphics.Layer;
 import mindustry.world.blocks.payloads.PayloadRouter;
 
 public class CoveredPayloadRouter extends PayloadRouter{
-    public TextureRegion coverRegion;
+    public TextureRegion iconRegion, coverRegion;
 
     public CoveredPayloadRouter(String name){
         super(name);
@@ -18,7 +20,19 @@ public class CoveredPayloadRouter extends PayloadRouter{
     public void load(){
         super.load();
 
+        iconRegion = Core.atlas.find(name + "-icon");
         coverRegion = Core.atlas.find(name + "-cover");
+    }
+
+    @Override
+    protected TextureRegion[] icons(){
+        return new TextureRegion[]{iconRegion, coverRegion};
+    }
+
+    @Override
+    public void drawPlanRegion(BuildPlan plan, Eachable<BuildPlan> list){
+        Draw.rect(iconRegion, plan.drawx(), plan.drawy(), plan.rotation * 90);
+        Draw.rect(coverRegion, plan.drawx(), plan.drawy());
     }
 
     public class CoveredPayloadRouterBuild extends PayloadRouterBuild{
