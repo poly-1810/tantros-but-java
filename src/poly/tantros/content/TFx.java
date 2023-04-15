dd33e4336958fdb2c79a1128258b8fd359d5ad45
MeiNanziiii <oplotDev@gmail.com>
2023-04-15 10:41:40 +0300

    code clear

--- src/poly/tantros/content/TFx.java
+++ src/poly/tantros/content/TFx.java
@@ -1,16 +1,13 @@
 package poly.tantros.content;
 
 import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.util.*;
 import mindustry.entities.*;
 import mindustry.graphics.*;
-import mindustry.world.blocks.payloads.*;
 
 import static arc.graphics.g2d.Draw.color;
 
 public class TFx{
-    public static Effect
+    public static final Effect
 
     siftDust = new Effect(20f, e -> {
         color(e.color, e.fout(0.1f));
